7df3500b0ac14ba841a80c15cb4285658d52be4f
MarcosGuii7 <contato.marcosguilherme.dsg@gmail.com>
2025-04-09 16:40:36 -0300

    Add files via upload

---
+++ www/Aula_1/www/Aula_1/divisao.js
@@ -0,0 +1,7 @@
+// código para exibir váriavel
+var valor_1 = 100
+var valor_2 = 10
+var divisao = valor_1 / valor_2
+
+// código para exibir o valor da frase + variável
+console.log("A divisão dos algarismos é: " + divisao)
---
+++ www/Aula_1/www/Aula_1/soma_algarismos.js
@@ -0,0 +1,7 @@
+// código para exibir váriavel
+var valor_1 = 7
+var valor_2 = 21
+var soma = (valor_1 + valor_2)
+
+// código para exibir o valor da frase + variável
+console.log("A soma dos algarismos é: " + soma)
