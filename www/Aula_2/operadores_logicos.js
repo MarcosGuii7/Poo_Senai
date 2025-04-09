cb66311a8f3878c6ed978570b7933ee91302908d
MarcosGuii7 <contato.marcosguilherme.dsg@gmail.com>
2025-04-09 16:41:19 -0300

    Add files via upload

---
+++ www/Aula_2/operadores_logicos.js
@@ -0,0 +1,25 @@
+// Igualdade de valor (==)
+console.log(5==5);
+
+//diferença de valor (!=)
+console.log(5!=5);
+
+//maior que (>)
+console.log(10 > 7);
+
+//Menor que (<)
+console.log(3 < 5);
+
+//Maior ou igual a (>=)
+console.log(7 >= 7);
+
+//Menor ou igual a (<=)
+console.log(4 <= 9);
+
+// E (&&)
+console.log(5 == 5 && 10 > 7);
+
+//Ou (||)
+console.log(5 == 5 || 5 > 7);
+
+//Não (!)
